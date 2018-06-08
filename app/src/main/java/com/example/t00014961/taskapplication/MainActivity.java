6a9169ff7cf91298932c262ee03446d1ef8ba009
atomlinson13 <a.tomlinson13@hotmail.com>
2018-06-07 18:44:59 -0700

    commit

--- app/src/main/java/com/example/t00014961/taskapplication/MainActivity.java
+++ app/src/main/java/com/example/t00014961/taskapplication/MainActivity.java
@@ -20,67 +20,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private  DrawerLayout mDrawerLayout;
+    private DrawerLayout mDrawerLayout;
     Button btn1;
     String[] tasks;
 
     private TextView mTextMessage;
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_tasks:
-//                    mTextMessage.setText(R.string.title_tasks);
-//                    return true;
-//                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-//            }
-//            return false;
-//        }
-//    };
-//
-//    public MainActivity(lateinit var) {
-//        this.var = var;
-//    }
-private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // btn1 = (Button) findViewById(R.id.button);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.navigations);
 
         final ListView lv = findViewById(R.id.list);
-     //   final Button btn = (Button) findViewById(R.id.btn);
 
-        // Initializing a new String Array
-        String[] fruits = new String[] {
-                "Cape Gooseberry",
-                "Capuli cherry"
-        };
-
-// ...
         mAuth = FirebaseAuth.getInstance();
         tasks = getResources().getStringArray(R.array.tasks);
 
 
         // Create a List from String Array elements
-        final List<String> fruits_list = new ArrayList<String>();
 
         // Create an ArrayAdapter from List
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, tasks);
+                (this, R.layout.multi, tasks);
 
-        // DataBind ListView with items from ArrayAdapter
-        lv.setAdapter(arrayAdapter);
 
 
         navigationView.setNavigationItemSelectedListener(
@@ -92,64 +58,12 @@ private FirebaseAuth mAuth;
                         return true;
                     }
                 });
+
+
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        lv.setAdapter(arrayAdapter);
+
     }
-
-    public void navClick(View view) {
-        mDrawerLayout.closeDrawers();
-
-
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(FirebaseUser currentUser) {
-    }
-
-
-//        mDrawerLayout.addDrawerListener(
-//                new DrawerLayout.DrawerListener() {
-//                    @Override
-//                    public void onDrawerSlide(View drawerView, float slideOffset) {
-//                        // Respond when the drawer's position changes
-//                    }
-//
-//                    @Override
-//                    public void onDrawerOpened(View drawerView) {
-//                        // Respond when the drawer is opened
-//                    }
-//
-//                    @Override
-//                    public void onDrawerClosed(View drawerView) {
-//                        // Respond when the drawer is closed
-//                    }
-//
-//                    @Override
-//                    public void onDrawerStateChanged(int newState) {
-//                        // Respond when the drawer motion state changes
-//                    }
-//                }
-//        );
-
-
-
-   // }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
 
 }
